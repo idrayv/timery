03e38bd54b3b13aec62fb88e2a39586fa6bf3d40
Maksym <drayv.mj12@gmail.com>
2019-01-09 20:55:25 +0200

    Daily notes table deleted

--- aspnet-core/Timery.EntityFrameworkCore/TimeryDbContext.cs
+++ aspnet-core/Timery.EntityFrameworkCore/TimeryDbContext.cs
@@ -10,8 +10,6 @@ namespace Timery.EntityFrameworkCore
 
         public DbSet<Event> Events { get; set; }
 
-        public DbSet<DailyNote> DailyNotes { get; set; }
-
         public TimeryDbContext()
         {
             Database.EnsureCreated();
